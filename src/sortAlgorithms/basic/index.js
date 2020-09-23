25838576969167000848f590e444b7f277418be7
jm-avila <jm.avila1309@icloud.com>
2020-09-23 10:24:23 +0200

    Beginning of merge sort

--- src/sortAlgorithms/basic/index.js
+++ src/sortAlgorithms/basic/index.js
@@ -1,5 +1,3 @@
-const bubbleSort = require("./");
+const bubbleSort = require("./bubbleSort");
 const selectionSort = require("./selectionSort");
 const insertionSort = require("./insertionSort");
-
-console.log(insertionSort([2, 1, 9, 76, 4]));
